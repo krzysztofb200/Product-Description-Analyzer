3052a7215ac424ba898cd0737bc61bf87a7c08f2
Krzysztof Baczyński <k.baczynski200@gmail.com>
2022-12-31 19:37:30 +0100

    - login activity added

--- app/src/main/java/com/example/project_ver1/MainActivity.java
+++ app/src/main/java/com/example/project_ver1/MainActivity.java
@@ -96,6 +96,13 @@ public class MainActivity extends AppCompatActivity {
         TextView navEmail = (TextView) headerView.findViewById(R.id.nav_email);
         navUsername.setText(name);
         navEmail.setText(email);
+
+        // Ukrycie jednego z elementow navigation drawer
+//        navigationView = (NavigationView) findViewById(R.id.nav_view);
+//        Menu nav_Menu = navigationView.getMenu();
+//        nav_Menu.findItem(R.id.nav_shopping_list).setVisible(false);
+
+        // TODO: Logout button, add product activity visible only for logged users, add product to favorites
     }
 
     @Override
