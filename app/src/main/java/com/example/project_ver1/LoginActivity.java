7a0cc663d1323b737228c6aec6a18045dc2d7eb2
Krzysztof Baczyński <k.baczynski200@gmail.com>
2024-01-13 13:15:02 +0100

    - login activity

--- app/src/main/java/com/example/project_ver1/LoginActivity.java
+++ app/src/main/java/com/example/project_ver1/LoginActivity.java
@@ -97,4 +97,9 @@ public class LoginActivity extends AppCompatActivity {
                             }
                         });
     }
+
+    @Override
+    public void onBackPressed() {
+        moveTaskToBack(true);
+    }
 }
